<fix> masking value bug fix ...

src/model.py
@@ -32,7 +32,7 @@ class Attention(nn.Module):
                 1, 1, max_seq_len, max_seq_len
             )
         )
-        self.register_buffer("masking_value", torch.tensor(1e-4))
+        self.register_buffer("masking_value", torch.tensor(-1e4))
 
         self.wqkv = nn.Linear(in_features=emb_dim, out_features=emb_dim * 3)
         self.fc = nn.Linear(in_features=emb_dim, out_features=emb_dim)

src/utils/generate.py
@@ -79,9 +79,9 @@ def generate(
             logits = model(input_ids).squeeze(0) # (seq_len, vocab_size)
             pred = generate_fn(logits) # single index
             
-            pred_ids.append(pred.item())
             if(pred.item() == tokenizer.vocab.eos_token_id):
                 break
+            pred_ids.append(pred.item())
 
             input_ids = torch.cat((input_ids, pred.view(1, 1)), dim=-1)
 